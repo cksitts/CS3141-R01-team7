dbe01c93f2763536b682abf3c7522e0505bf1331
emilierummer <114100983+emilierummer@users.noreply.github.com>
2022-10-15 12:39:54 -0400

    Emilie start commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
-l_app = Flask(__name__);
+l_app = Flask(__name__)
+l_app.secret_key = "b3mex0c5xd7^x8c/xdcv#xbfkBq1)xb4C`>xffqu"
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,166 @@
+from symbol import decorator
+from flask import render_template, redirect, url_for
+from flask import request, session
+from functools import wraps
 from app import l_app
+
+#allows us require someone is logged in to get to a certain page
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('username'):
+            return redirect(url_for('index'))
+        return f(*args, **kwargs)
+    return decorated_function  
+
+
 
 @l_app.route('/')
 @l_app.route('/index')
-def index():
-    return 'default.';
+@l_app.route('/index/<validLogin>')
+def index(validLogin=True):
+    return render_template('pages/index.html', validLogin=validLogin)
+
+
+
+@l_app.route('/login', methods=['GET','POST'])
+def login():
+    #TODO check if login valid:
+    #request.form['username']
+    #request.form['password']
+    loginValid = True
+    
+    if(loginValid):
+        #TODO use email
+        session['username'] = request.form['username'] #will be used to validate that someone is logged in
+        return redirect(url_for('home'))
+    else:
+        return redirect(url_for('index', validLogin=False)) #show invalid login error and have then try again
+
+
+
+@l_app.route('/logout')
+@login_required
+def logout():
+    session.pop('username', default=None)
+    return redirect(url_for('index'))
+
+
+
+@l_app.route('/signup')
+def signup():
+    #TODO pull list of laundry rooms from database
+    roomList = ['G23E Wads (Ground floor east)','134E Wads (First floor east)','154W Wads (First floor west)']
+
+    return render_template('pages/accountInfo.html', requestType='signup', roomList=roomList)
+
+
+
+@l_app.route('/signup/request', methods=['GET','POST'])
+def signupRequest():
+    #TODO save new account to database
+    #request.form['email']
+    #request.form['username']
+    #request.form['password']
+    #request.form['passwordConfirm']
+
+    return redirect(url_for('home')) #redirect to home page
+
+
+
+@l_app.route('/passwordreset')
+@login_required
+def passwordReset():
+    #TODO whole method
+    return "password reset"
+
+
+
+@l_app.route('/editaccount')
+@login_required
+def editAccount():
+    #TODO pull list of laundry rooms from database
+    roomList = ['G23E Wads (Ground floor east)','134E Wads (First floor east)','154W Wads (First floor west)']
+
+    #TODO get user data of person currently logged in
+    userData = {'email':'user@gmail.com','username':'testUser','password':'blegh','preferredRoom':'154W Wads (First floor west)'}
+
+    return render_template('pages/accountInfo.html', requestType='edit', userData=userData, roomList=roomList)
+
+
+
+@l_app.route('/editaccount/request', methods=['GET','POST']) 
+@login_required  
+def editAccountRequest():
+    #TODO save new details to database
+    #request.form['oldEmail']
+    #request.form['email']
+    #request.form['username']
+    #request.form['password']
+    #request.form['passwordConfirm']
+
+    return redirect(url_for('home')) #redirect to home page
+
+
+
+@l_app.route('/home')
+@login_required
+def home():
+    #TODO pull actual data from database
+    #TODO format better based on how database works
+    allMachines = [
+            {
+                'code':'134EWH1',
+                'location':'134E Wads (First floor east)',
+                'number':1,
+                'type':'washer',
+                'available':False,
+                'time-remaining':30
+            },
+            {
+                'code':'134EWH2',
+                'location':'134E Wads (First floor east)',
+                'number':2,
+                'type':'washer',
+                'available':False,
+                'time-remaining':20
+            },
+            {
+                'code':'134EWH3',
+                'location':'134E Wads (First floor east)',
+                'number':1,
+                'type':'dryer',
+                'available':False,
+                'time-remaining':60
+            },
+            {
+                'code':'154WWH1',
+                'location':'154W Wads (First floor west)',
+                'number':1,
+                'type':'washer',
+                'available':True,
+                'time-remaining':0
+            },
+            {
+                'code':'154WWH2',
+                'location':'154W Wads (First floor west)',
+                'number':2,
+                'type':'washer',
+                'available':True,
+                'time-remaining':0
+            },
+            {
+                'code':'154WWH3',
+                'location':'154W Wads (First floor west)',
+                'number':3,
+                'type':'washer',
+                'available':False,
+                'time-remaining':10
+            }
+    ]
+    userMachines = ['134EWH1','134EWH2','154WWH2']
+    
+    #TODO pull list of laundry rooms from database
+    roomList = ['G23E Wads (Ground floor east)','134E Wads (First floor east)','154W Wads (First floor west)']
+    
+    return render_template('pages/home.html', userMachines=userMachines, allMachines=allMachines, laundryRoomList=roomList)
