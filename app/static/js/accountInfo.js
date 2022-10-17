388deb63dd7d5006b34194d3c4943451f14b2588
Jacob Suiter <jasuiter@mtu.edu>
2022-10-17 19:26:16 -0400

    Update accountInfo.js
    Updated the page error to reflect the conditions better

--- app/static/js/accountInfo.js
+++ app/static/js/accountInfo.js
@@ -10,7 +10,7 @@ function validateForm() {
     //Regex used: any combination of letters, numbers, and underscores. Min length 6 characters.
     const usernameRegex = /^[\w]{6,}$/
     if(!usernameRegex.test(document.getElementById('usernameInput').value)) {
-        alert("Invalid username, please do not use special characters (minimum length 1)")
+        alert("Invalid username, please do not use special characters (minimum length 6)")
         return false
     }
     //TODO check if username is already taken in the database
@@ -23,7 +23,7 @@ function validateForm() {
     //Regex used: any combination of letters, numbers, and special characters. Min length 8 characters.
     const passwordRegex = /^.{8,}$/
     if(!passwordRegex.test(document.getElementById('passwordInput').value)) {
-        alert("Invalid password, please do not use special characters (minimum length 1)")
+        alert("Invalid password, please do not use special characters (minimum length 8)")
         return false
     }
 
