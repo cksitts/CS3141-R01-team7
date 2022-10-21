b64deddeca7b5665fac31e549ef2a381c80894c5
emilierummer <114100983+emilierummer@users.noreply.github.com>
2022-10-20 20:43:27 -0400

    Profile Info Mouseover
    Updated profile info to display on mouse over instead of on click

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -1,19 +1,18 @@
-function toggleDropdown() {
-    if(getComputedStyle(document.getElementById('profileDropdown')).getPropertyValue("display") == 'block') {
-        document.getElementById('profileDropdown').style.animationName = 'dropupAnimation';
-        document.getElementById('profileDropdown').style.animationPlayState = 'running';
+function dropdownDown() {
+    document.getElementById('profileDropdown').style.animationName = 'dropdownAnimation';
+    document.getElementById('profileDropdown').style.animationPlayState = 'running';
 
-        setTimeout("document.getElementById('profileDropdown').style.display = 'none';", 500);
-        
-        //button style
-        document.getElementById('dropdownButton').style.border = 'none';
-    } else {
-        document.getElementById('profileDropdown').style.animationName = 'dropdownAnimation';
-        document.getElementById('profileDropdown').style.animationPlayState = 'running';
+    setTimeout("document.getElementById('profileDropdown').style.display = 'block';", 500);
+    
+    //button style
+    document.getElementById('dropdownButton').style.border = '2px solid var(--main-dark)';
+}
+function dropdownUp() {
+    document.getElementById('profileDropdown').style.animationName = 'dropupAnimation';
+    document.getElementById('profileDropdown').style.animationPlayState = 'running';
 
-        setTimeout("document.getElementById('profileDropdown').style.display = 'block';", 500);
-        
-        //button style
-        document.getElementById('dropdownButton').style.border = '2px solid var(--main-dark)';
-    }
+    setTimeout("document.getElementById('profileDropdown').style.display = 'none';", 500);
+    
+    //button style
+    document.getElementById('dropdownButton').style.border = 'none';
 }
