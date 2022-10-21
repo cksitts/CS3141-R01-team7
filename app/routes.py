e39b0706b0361576157293f48f5b563e6bbf64c4
ginganinja546 <tlsorens@mtu.edu>
2022-10-21 16:10:35 -0400

    Add email verification
    Email verification is added and almost completely functional. There are some problem with inserting into the database but otherwise everything is working.

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from re import L
 from symbol import decorator
 from functools import wraps
 
@@ -7,6 +8,11 @@ from flask import request, session
 from app import l_app
 from app import mysql
 
+import app.static.python.signup_helper as signupHelper
+
+# used for debugging
+import sys
+
 #allows us require someone is logged in to get to a certain page
 def login_required(f):
     @wraps(f)
@@ -15,7 +21,6 @@ def login_required(f):
             return redirect(url_for('index'))
         return f(*args, **kwargs)
     return decorated_function  
-
 
 
 @l_app.route('/')
@@ -56,37 +61,70 @@ def signup(emailTaken=False):
 
     return render_template('accountInfo.html', requestType='signup', roomList=roomList, emailTaken=emailTaken)
 
-
-
 @l_app.route('/signup/request', methods=['GET','POST'])
 def signupRequest():
-    # if the user tries to redirect with a link, send them back to the
-    # original page that they were at.
+    # if the user tries to access this page with a GET, redirect to /signup
+    if (request.method == 'GET'):
+        return redirect(url_for('signup'))
 
-    #TODO save new account to database
+    # get the email and username from the form
     email = request.form['email']
     username = request.form['username']
-    request.form['password']
 
     # connect to the database with cursor
+    # TODO use ACID transactions to help with concurrent queries
+    #   - set transaction isolation level serializable
     cursor = mysql.connection.cursor()
 
     # check the database to see if the email already exists
-    cursor.execute( '''SELECT * FROM MachineUser WHERE email="%s"''' % str(email) )
-    temp = cursor.fetchall()
-
-    if (len(temp) != 0): 
+    cursor.execute( '''SELECT * FROM MachineUser WHERE email=%s''', (str(email),) )
+    if (len(cursor.fetchall()) != 0): 
         return redirect(url_for('signup', emailTaken=True))
 
-    # if the email doesn't exist, validate the email
-    # TODO create, then pull this from a supplementary python script used for
-    # methods that help the routes file in the backend
-    
-    # close database connection
+    # First, salt and hash the password and store all of the info in the database
+    # the password is only now pulled from the form
+    pass_hash = signupHelper.generateHashAndSalt(str(request.form['password']))
+
+    print(email, file=sys.stdout)
+    cursor.execute( ('''INSERT INTO MachineUser VALUES (%s, %s, %s, %s)'''), 
+                    (str(email), str(username), str(pass_hash[0]), str(pass_hash[1])) )
+
+    # close the database connection and validate the email
     cursor.close()
+    return redirect(url_for('verifyEmail', email=str(email), count=1))
 
-    return redirect(url_for('home')) #redirect to home page
+@l_app.route('/verifyemail', methods = ['GET', 'POST'])
+@l_app.route('/verifyemail/<email>/<count>', methods = ['GET', 'POST'])
+def verifyEmail(email, count):
+    
+    # if we are sending a GET
+    if (request.method == 'GET'):
+        verification_code = signupHelper.sendSignupEmail(email)
+        session['verification_code'] = verification_code
+        print(session.get('verification_code'), file=sys.stdout)
+        return render_template('email_verifier.html', email=email, count=count)
 
+    if (request.method == 'POST'):
+        print(session.get('verification_code'), file=sys.stdout)
+        # get the current number of attempts
+        attempt_count = request.form['count']
+
+        # if the correct code is input
+        if (session.get('verification_code') == str(request.form['codeInput'])):
+            return redirect(url_for('index'))
+
+        # if we still haven't passed 3 attempts let another attempt happen
+        _count = int(attempt_count)
+        if (_count <= 3):
+            _count += 1
+            return redirect(url_for('verifyEmail', email=email, count=_count))
+
+        # If unsuccessful, open a connection to the database and remove the tuple for "email"
+        # Then redirect to the sign up page
+        cursor = mysql.connection.cursor()
+        cursor.execute( '''DELETE FROM MachineUser WHERE email=%s''', (str(email),) )
+        cursor.close()
+        return redirect(url_for('signup'))
 
 
 @l_app.route('/passwordreset')
@@ -94,7 +132,6 @@ def signupRequest():
 def passwordReset():
     #TODO whole method
     return "password reset"
-
 
 
 @l_app.route('/editaccount')
---
+++ app/static/python/signup_helper.py
@@ -0,0 +1,40 @@
+# File holds the methods that help with the signup process
+from app.static.python.send_email import sendEmail
+import secrets
+from hashlib import sha256
+
+"""
+Function to send an email with a verification code specifically for signups
+@param to: the address to send the message to
+@return the code that was sent so that it can be verified
+"""
+def sendSignupEmail(to):
+    # generate a random verification code using a random hex sequence
+    # the length of the sequence is 6 total digits (3 bytes converted to 2 digits each)
+    code_length = 3
+    code = str(secrets.token_hex(code_length)).upper()
+    sendEmail("Enter Verification Code: %s" % code, to)
+    return code
+
+"""
+Generate a password's salt, then hash it
+@param password: the password to hash
+@return a tuple with the hash and the salt added before hashing
+"""
+def generateHashAndSalt(password):
+    # generate a salt string
+    length = secrets.randbelow(11) + 5                           # random length in [5,15]
+    char_offset = 0
+    printable_char_code = 65;                                    # character from ascii 65 = A
+    
+    salt = ""
+    
+    for c in range(0, length + 1):
+        char_offset = secrets.randbelow(90);                     # choose a random character 
+        new_char = chr(printable_char_code + char_offset);       # store that character
+        salt += new_char;                                        # add that character to the salt sequence
+
+    # hash the password with the salt string
+    _hash = sha256( str(password + salt).encode() )
+
+    return [_hash.hexdigest(), salt]                             # return the tuple
