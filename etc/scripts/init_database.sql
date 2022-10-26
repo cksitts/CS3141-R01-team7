0c8d3708c1ef0a5e7ec704ba9faed5cfa5b66a86
ginganinja546 <tlsorens@mtu.edu>
2022-10-26 00:57:11 -0400

    Update init_database.sql

--- etc/scripts/init_database.sql
+++ etc/scripts/init_database.sql
@@ -1,22 +1,26 @@
--- set the password in the database to the one we are using
-alter user 'root'@'localhost' identified by 'MRy_0VE9ARuv2zgcgaeepw';
-
 -- setup the database
-create database laundry_tracker_db;
+create database if not exists laundry_tracker_db;
 
 -- setup the tables in the new database
 use laundry_tracker_db;
 
+set foreign_key_checks = 0;
+drop table if exists MachineUser;
+drop table if exists UsingMachine;
+drop table if exists Machine;
+set foreign_key_checks = 1;
+
 create table MachineUser (	email varchar(64) primary key not null, 
 							username char(64) not null, 
                             pass_hash char(64) not null, 
-                            pass_salt char(10) not null );
+                            pass_salt char(15) not null );
 
-create table Machine (	machine_id varchar(64) primary key not null, 
-						location varchar(10) not null,
+create table Machine (	machine_id char(10) primary key not null, 
+						location varchar(32) not null,
                         machine_type char(8) not null );
 
-create table UsingMachine (	machine_id varchar(64) primary key not null, 
+
+create table UsingMachine (	machine_id char(10) primary key not null, 
 							email varchar(64) not null, 
 							time_started time not null, 
 							foreign key (machine_id) references Machine(machine_id),
