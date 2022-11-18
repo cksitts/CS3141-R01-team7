d469a6bc48f9ef0408e7a30d4dc472e2024fa3d8
ginganinja546 <tlsorens@mtu.edu>
2022-11-18 12:35:49 -0500

    Toggle db connection
    Boolean use_local_database now determines which database the flask app attempts to configure with.

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,26 @@ import os
 l_app = Flask(__name__)
 l_app.secret_key = os.environ.get('TSP_SECRET_KEY')
 
+
 # create the mysql parent object
-mysql = conn.connect(   user='flask', 
-                        password=os.environ.get('TSP_DB_PASS'),
-                        host=os.environ.get('TSP_DB_HOST'),
-                        port=3306,
-                        database='laundry_tracker_db',
-                        ssl_ca='./etc/DigiCertGlobalRootCA.crt.pem',
-                        ssl_disabled=False    )
+# use the local db if this is true
+use_local_database = True
+
+if (use_local_database):
+    mysql = conn.connect(   user='root', 
+                            password=os.environ.get('TSP_DB_PASS'),
+                            host='localhost',
+                            port=3306,
+                            database='laundry_tracker_db'   )
+else:
+    mysql = conn.connect(   user='flask', 
+                            password=os.environ.get('TSP_DB_PASS'),
+                            host=os.environ.get('TSP_DB_HOST'),
+                            port=3306,
+                            database='laundry_tracker_db',
+                            ssl_ca='./etc/DigiCertGlobalRootCA.crt.pem',
+                            ssl_disabled=False    )
+
+
 
 from app import routes
