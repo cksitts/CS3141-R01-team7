61c0ff5eb793f457ca82d06f737b5884f45bd0d7
emilierummer <114100983+emilierummer@users.noreply.github.com>
2022-12-08 16:56:36 -0500

    Fixed running animation bug and added progress bar live updates

--- app/static/js/updateAnimation.js
+++ app/static/js/updateAnimation.js
@@ -1,21 +1,34 @@
-function updateAnimation(imgElement) {
-    if(imgElement.classList.contains('running')) {
-        //stop running animation
-        imgElement.classList.remove('running')
-        if(imgElement.getAttribute('src') == "/static/images/running_washer.gif") {
-            imgElement.src = "/static/images/full_washer.png";
-        } else if(imgElement.getAttribute('src') == "/static/images/running_dryer.gif") {
-            imgElement.src = "/static/images/full_dryer.png";
+setInterval(function updateProgressBar() {
+    document.querySelectorAll('progress.timeRemaining').forEach(bar => {
+        if(bar.value < 60) {
+            bar.value = bar.value + 1
+        } else {
+            stopRunningAnimation(bar.parentElement.querySelector('img'))
         }
-    } else {
-        //start running animation
-        imgElement.classList.add('running')
+    }) 
+    document.querySelectorAll('p.timeRemaining').forEach(p => {
+        var timeRemaining = p.innerHTML.split(' ')[0]
+        if(timeRemaining > 0) {
+            p.innerHTML = `${timeRemaining - 1} minutes remaining`
+        } else {
+            p.innerHTML = "Done"
+        }
+    })
+}, 60*1000)
+
+function startRunningAnimation(imgElement) {
+    imgElement.classList.add('running')
         if(imgElement.getAttribute('src') == "/static/images/full_washer.png") {
             imgElement.src = "/static/images/running_washer.gif";
         } else if(imgElement.getAttribute('src') == "/static/images/full_dryer.png") {
             imgElement.src = "/static/images/running_dryer.gif";
         }
-    }
 }
-
-                        
+function stopRunningAnimation(imgElement) {
+    imgElement.classList.remove('running')
+        if(imgElement.getAttribute('src') == "/static/images/running_washer.gif") {
+            imgElement.src = "/static/images/full_washer.png";
+        } else if(imgElement.getAttribute('src') == "/static/images/running_dryer.gif") {
+            imgElement.src = "/static/images/full_dryer.png";
+        }
+}
